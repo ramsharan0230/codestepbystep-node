e3ebe8d4488a8efe61c8da6b28b74d76694625e9
ramsharan <ramsharan0230@gmail.com>
2022-02-23 16:22:13 +0545

    updated middle for route group

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const path = require('path');
 const reqFilter = require('./middleware')
 
 const app = express()
+const route = express.Router()
 
 const pathName = path.join(__dirname, 'views')
-
+route.use(reqFilter)
 // app.use(express.static(pathName))
 app.set('view engine', 'ejs')
 
@@ -14,13 +15,19 @@ app.get('/', (req, res)=>{
     res.render(`${pathName}/index`)
 })
 
-app.get('/home', reqFilter, (req, res)=>{
+app.get('/home', (req, res)=>{
     res.render('home')
 })
 
-app.get('/about', (req, res)=>{
+route.get('/about', (req, res)=>{
     res.render(`${pathName}/about`)
 })
+
+route.get('/contact', (req, res)=>{
+    res.render(`${pathName}/contact`)
+})
+
+app.use('/', route)
 
 app.get('*', (req, res)=>{
     res.render(`${pathName}/404`)
