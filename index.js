670258287cfca2f499c44e0b7772ee17174e0cbf
ramsharan <ramsharan0230@gmail.com>
2022-02-24 21:23:50 +0545

    basic mongoose

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const products = require('./product')
-const productCreate = require('./product/create')
-const productEdit = require('./product/edit')
+const mongoose = require('mongoose');
 
-// productCreate();   
-products.products()
-// products.productDelete(); 
-// productEdit()
+const main = async() =>{
+    await mongoose.connect('mongodb://127.0.0.1:27017/ecomm_dbase');
+
+    const productSchema = new mongoose.Schema({
+        title: {
+            type: String,
+            unique: true
+        },
+        memory: String,
+        category: String,
+        color: Array,
+        model: String,
+        manufacture: {
+            type: Date
+        },
+        price: Number,
+        description: String
+    });
+
+    const productModel = mongoose.model('products', productSchema);
+    let data = new productModel({
+        title: "Nokia 512",
+        memory: "256 GB",
+        category: "Mobile",
+        color: ['black', 'red', 'maroon'],
+        model: 'nokia512256gb',
+        manufacture: new Date(),
+        price: 900,
+        description: "This is the Nokia best phone ever!"
+    });
+
+    let result = await data.save();
+    console.log(result);
+}
+
+main();
+
 
