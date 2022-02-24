57fe802065424aad104e1bd7a19accd65ff327d8
ramsharan <ramsharan0230@gmail.com>
2022-02-24 20:50:57 +0545

    api updated crud

--- index.js
+++ index.js
@@ -1,9 +1,49 @@
-const products = require('./product')
-const productCreate = require('./product/create')
-const productEdit = require('./product/edit')
+const express = require('express')
+const mongodb = require('mongodb')
+const app = express()
+const dbConn = require('./mongodb')
 
-// productCreate();   
-products.products()
-// products.productDelete(); 
-// productEdit()
+app.use(express.json())
 
+//get all products
+app.get('/', async(req, res)=>{
+    let conn = await dbConn();
+    let collection = await conn.collection('products');
+    let data = await collection.find().toArray()
+
+    res.send(data);
+})
+
+//post product
+app.post('/', async(req, res)=>{
+    let conn = await dbConn();
+    let collection = await conn.collection('products');
+    let result = await collection.insert(req.body)
+    res.send(result)
+})
+
+//update product
+app.put('/', async(req, res)=>{
+    let conn = await dbConn();
+    let collection = await conn.collection('products');
+    let result = await collection.update(
+        { model: req.body.model },{
+            $set:req.body
+        }
+    )
+    res.send(result)
+})
+
+//delete product
+app.delete('/:id', async(req, res)=>{
+    let conn = await dbConn();
+    let collection = await conn.collection('products');
+    console.log(req.params.id)
+
+    let result = await collection.deleteOne(
+        { _id: new mongodb.ObjectId(req.params.id)}
+    )
+    res.send(result)
+})
+
+app.listen(8080)
